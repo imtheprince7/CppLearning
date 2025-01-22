a371da48ab50f4f1c330cf34ffbe717510c36784
imtheprince7 <princeks3577@gmail.com>
2025-01-22 16:45:18 +0530

    leetcode Tree

---
+++ Data Structure/Tree/Leetcode22Jan.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+    public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == nullptr)
+      return 0;
+
+    int ans = 0;
+    stack<TreeNode*> stack{{root}};
+
+    while (!stack.empty()) {
+      root = stack.top(), stack.pop();
+      if (root->left) {
+        if (root->left->left == nullptr && root->left->right == nullptr)
+          ans += root->left->val;
+        else
+          stack.push(root->left);
+      }
+      if (root->right)
+        stack.push(root->right);
+    }
+
+    return ans;
+    }
+};
+int main(){
+    Solution solution;
+    // cout << solution.sumOfLeftLeaves()
+
+    return 0;
+}
